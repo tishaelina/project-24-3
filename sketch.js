c5dd21619533d5eee47136616ea40268769450f2
tishaelina <85218242+tishaelina@users.noreply.github.com>
2021-07-11 12:02:53 +0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,93 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var canvas;
+var palyer, playerBase;
+var computer, computerBase;
+var computerArcher, playerArcher;
+var arrow;
+var playerArrow,computerArrow;
+
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+
+  engine = Engine.create();
+  world = engine.world;
+
+  playerBase = new PlayerBase(300, random(450, height - 300), 180, 150);
+  player = new Player(285, playerBase.body.position.y - 153, 50, 180);
+ 
+   playerArcher = new PlayerArcher(
+   width - 1010,
+   playerBase.body.position.y -180,
+   120,
+   120
+  );
+
+  computerBase = new ComputerBase(
+    width - 300,
+    random(450, height - 300),
+    180,
+    150
+  );
+  computer = new Computer(
+    width - 280,
+    computerBase.body.position.y - 153,
+    50,
+    180
+  );
+  computerArcher = new ComputerArcher(
+    width - 340,
+    computerBase.body.position.y - 180,
+    120,
+    120
+  );
+  
+  playerArrow = new PlayerArrow(
+    playerArcher.body.position.x,
+    playerArcher.body.position.y,
+    100,10
+  );
+  
+  
+}
+
+function draw() {
+  background(180);
+
+  Engine.update(engine);
+
+  // Title
+  fill("#FFFF");
+  textAlign("center");
+  textSize(40);
+  text("EPIC ARCHERY", width / 2, 100);
+
+ 
+  playerBase.display();
+  player.display();
+  
+
+  computerBase.display();
+  computer.display();
+  
+  playerArcher.display();
+  computerArcher.display();
+
+
+  playerArrow.display();
+  //computerArrow.display();
+  
+  //if Space (32) key is pressed call shoot function of playerArrow
+  if(keyCode === 32){
+    playerArrow.shoot(playerArcher.body.angle)
+  }
+  
+}
+
+
+
